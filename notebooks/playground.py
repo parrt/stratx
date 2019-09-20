01870e4bef4e06503016ee00a81db4a82dc7bd2d
parrt <parrt@cs.usfca.edu>
2019-09-20 15:58:05 -0700

    show synthetic data

--- notebooks/playground.py
+++ notebooks/playground.py
@@ -4,35 +4,42 @@ from stratx.featimp import *
 
 PLOT = True
 
-if PLOT:
-    boston = load_boston()
-    df = pd.DataFrame(boston.data, columns=boston.feature_names)
-    df['MEDV'] = boston.target
-    # df = df.head(50)
+def synthetic_poly_data(n, p, degree=1):
+    df = pd.DataFrame()
+    # Add independent x variables in [0.0, 1.0)
+    coeff = np.random.random_sample(size=p)*10 # get p random coefficients
+    coeff = np.array([5, 2, 10])
+    for i in range(p):
+        df[f'x{i+1}'] = np.random.random_sample(size=n)
+    # multiply coefficients x each column (var) and sum along columns
+    df['y'] = np.sum( [coeff[i]*df[f'x{i+1}'] for i in range(p)], axis=0 )
+    #TODO add noise
+    return df, coeff
 
-    X = df.drop('MEDV', axis=1)
-    y = df['MEDV']
+
+df, coeff = synthetic_poly_data(100,3)
+X = df.drop('y', axis=1)
+y = df['y']
+X = standardize(X)
+
+if PLOT:
 
     fig, ax = plt.subplots(1,1)
-    # WORKS ONLY WITH DATAFRAMES AT MOMENT
-    plot_stratpd(X, y, 'AGE', 'MEDV', yrange=(-10,10), ax=ax)
-    marginal_xranges, marginal_sizes, marginal_slopes, ignored = \
-        discrete_xc_space(X['AGE'], y)
+    xc = 'x2'
+    min_samples_leaf = 10
+    plot_stratpd(X, y, 'x1', 'y', ax=ax, min_samples_leaf=min_samples_leaf)
+    plot_stratpd(X, y, 'x2', 'y', ax=ax, min_samples_leaf=min_samples_leaf)
+    plot_stratpd(X, y, 'x3', 'y', ax=ax, min_samples_leaf=min_samples_leaf)
+    # marginal_xranges, marginal_sizes, marginal_slopes, ignored = \
+    #     discrete_xc_space(X['AGE'], y)
 
-    uniq_x = np.array(sorted(np.unique(X['AGE'])))
-
-    ax.scatter(uniq_x[:-1], marginal_slopes)
+    # uniq_x = np.array(sorted(np.unique(X[xc])))
+    # ax.scatter(uniq_x[:-1], marginal_slopes)
+    ax.set_title(f"Coeffs = {coeff}, xc={xc}")
     plt.tight_layout()
     plt.show()
 
-boston = load_boston()
-df = pd.DataFrame(boston.data, columns=boston.feature_names)
-df['MEDV'] = boston.target
-df = df.head(50)
-
-X = df.drop('MEDV', axis=1)
-y = df['MEDV']
-
 # WORKS ONLY WITH DATAFRAMES AT MOMENT
-importances(X, y, 'AGE')
+
+importances(X, y)
 
--- stratx/featimp.py
+++ stratx/featimp.py
@@ -1,24 +1,41 @@
 import numpy as np
 import pandas as pd
+from typing import Sequence
 
 from sklearn.ensemble import RandomForestRegressor
 
 from stratx.partdep import PD, discrete_xc_space
 
-def importances(X, y, colname,
+def standardize(df):
+    # standardize variables
+    Z = df.copy()
+    for colname in df.columns:
+        Z[colname] = (Z[colname] - np.mean(Z[colname])) / np.std(Z[colname])
+    return Z
+
+
+def importances(X:pd.DataFrame, y:pd.Series, colnames:Sequence=None,
                 ntrees=1, min_samples_leaf=10, bootstrap=False,
                 max_features=1.0,
                 verbose=False):
     """
     """
-    leaf_xranges, leaf_slopes, pdpx, pdpy, ignored = \
-        PD(X=X, y=y, colname=colname, ntrees=ntrees, min_samples_leaf=min_samples_leaf,
-           bootstrap=bootstrap, max_features=max_features, supervised=True,
-           verbose=verbose)
+    if colnames is None:
+        colnames = X.columns.values
 
-    marginal_xranges, marginal_sizes, marginal_slopes, ignored = \
-        discrete_xc_space(X[colname], y, verbose=verbose)
+    # standardize variables
+    Z = X.copy()
+    for colname in colnames:
+        Z[colname] = (Z[colname] - np.mean(Z[colname])) / np.std(Z[colname])
 
-    print(pdpx)
-    print(pdpy)
-    print(marginal_slopes)
+    for colname in colnames:
+        leaf_xranges, leaf_slopes, pdpx, pdpy, ignored = \
+            PD(X=Z, y=y, colname=colname, ntrees=ntrees, min_samples_leaf=min_samples_leaf,
+               bootstrap=bootstrap, max_features=max_features, supervised=True,
+               verbose=verbose)
+        print(pdpx)
+        print(pdpy)
+
+    # marginal_xranges, marginal_sizes, marginal_slopes, ignored = \
+    #     discrete_xc_space(X[colname], y, verbose=verbose)
+
