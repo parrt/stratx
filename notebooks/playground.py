94c7ca6b6acd6876a6a7a57129594c4d8ff4dcb6
parrt <parrt@cs.usfca.edu>
2019-09-23 14:52:28 -0700

    avg weighted by num y values at that x

--- notebooks/playground.py
+++ notebooks/playground.py
@@ -11,7 +11,8 @@ def synthetic_poly_data(n, p, degree=1):
     coeff = np.random.random_sample(size=p)*10 # get p random coefficients
     coeff = np.array([5, 2, 10])
     for i in range(p):
-        df[f'x{i+1}'] = np.random.random_sample(size=n)*10
+        df[f'x{i+1}'] = np.round(np.random.random_sample(size=n)*10,1)
+    #df['x3'] = df['x1']  # copy x1
     # multiply coefficients x each column (var) and sum along columns
     # df['y'] = np.sum( [coeff[i]*df[f'x{i+1}'] for i in range(p)], axis=0 )
     df['y'] = 5*df['x1'] + 2*df['x2'] + 3*df['x3']**2
@@ -31,12 +32,10 @@ if True:
     fig, ax = plt.subplots(1,1)
     xc = 'x2'
     min_samples_leaf = 5
-    plot_stratpd(X, y, 'x1', 'y', ax=ax, min_samples_leaf=min_samples_leaf)
-    plot_stratpd(X, y, 'x2', 'y', ax=ax, min_samples_leaf=min_samples_leaf)
-    plot_stratpd(X, y, 'x3', 'y', ax=ax, min_samples_leaf=min_samples_leaf)
+    for colname in X.columns:
+        print(colname)
+        plot_stratpd(X, y, colname, 'y', ax=ax, min_samples_leaf=min_samples_leaf)
     # ax.scatter(df['x'], df['sum_pd'], s=3, c='k')
-    # marginal_xranges, marginal_sizes, marginal_slopes, ignored = \
-    #     discrete_xc_space(X['AGE'], y)
 
     # uniq_x = np.array(sorted(np.unique(X[xc])))
     # ax.scatter(uniq_x[:-1], marginal_slopes)
--- stratx/featimp.py
+++ stratx/featimp.py
@@ -31,30 +31,36 @@ def importances(X:pd.DataFrame, y:pd.Series, colnames:Sequence=None,
 
     n, p = X.shape
     df = pd.DataFrame()
-    df['x'] = sorted(X.iloc[0:-1,0])
+    #df['x'] = sorted(X.iloc[0:-1,0])
     # pick any standardized variable (column) for shared x
     # ignore last x coordinate as we have no partial derivative data at the end
-    for colname in colnames:
+    avgs = np.zeros(shape=(p,))
+    for i, colname in enumerate(colnames):
         leaf_xranges, leaf_slopes, pdpx, pdpy, ignored = \
             PD(X=X, y=y, colname=colname, ntrees=ntrees, min_samples_leaf=min_samples_leaf,
                bootstrap=bootstrap, max_features=max_features, supervised=True,
                verbose=verbose)
-        df[f"pd_{colname}"] = np.abs(pdpy)
+        x = X[colname]
+        x_filtered = x[np.isin(x, pdpx)]
+        print(len(x), len(pdpx), len(x_filtered))
+        uniq_x_counts = np.unique(x_filtered, return_counts=True)[1]
+        # print(list(zip(pdpx,uniq_x_counts)))
+        avgs[i] = np.sum(np.abs(pdpy) * uniq_x_counts) / np.sum(uniq_x_counts)
+        # df[f"pd_{colname}"] = np.abs(pdpy)
 
     # TODO: probably should make smallest pd value 0 to shift all up from 0 lest
     # things cancel
 
 
-    df['sum_pd'] = df.iloc[:,1:].sum(axis=1)
+    # df['sum_pd'] = df.iloc[:,1:].sum(axis=1)
 
     # do ratios for importance
     # for colname in X.columns:
     #     df[f'I_{colname}'] = df[f'pd_{colname}'] / df[f'sum_pd']
 
-
-    print(df)
-    avgs = [np.mean(df[f"pd_{colname}"]) for colname in colnames]
-    avgs /= np.sum(avgs) # normalize to 0..1
+    # print(df)
+    # avgs = [np.mean(df[f"pd_{colname}"]) for colname in colnames]
+    # avgs /= np.sum(avgs) # normalize to 0..1
 
     I = pd.DataFrame(data={'Feature':colnames, 'Importance':avgs})
     I = I.set_index('Feature')
