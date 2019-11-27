e5d5e73a880d7a6421e92c05f7ac752e8d8adee5
parrt <parrt@cs.usfca.edu>
2019-11-27 10:17:03 -0800

    rm junk

--- notebooks/play_marginal.py
+++ notebooks/play_marginal.py
@@ -34,11 +34,10 @@ def synthetic_poly_data(n, p):
     # coeff = np.array([5,10])
     for j in range(p):
         df[f'x{j+1}'] = np.round(np.random.random_sample(size=n)*1,1) # shift x_i to right 2
-    df['x3'] = df['x1']*np.round(np.random.random_sample(size=n),1) # copy x1 + noise
+    #df['x3'] = df['x1']*np.round(np.random.random_sample(size=n),1) # copy x1 + noise
     # multiply coefficients x each column (var) and sum along columns
     yintercept = 10
     df['y'] = np.sum( [coeff[i]*df[f'x{i+1}'] for i in range(p)], axis=0 ) + yintercept
-    #TODO add noise
     terms = [f"{coeff[i]:.1f}x_{i+1}" for i in range(p)] + [f"{yintercept:.0f}"]
     eqn = "y = " + '+'.join(terms)
     return df, coeff, eqn
@@ -55,29 +54,24 @@ def impact_importances(X: pd.DataFrame,
     stddevs = np.zeros(shape=(p,))
     total_dy_mass = 0.0
     for j, colname in enumerate(X.columns):
-        # Ask stratx package for the partial derivative of y with respect to X[colname]
+        # Ask stratx package for the partial dependence of y with respect to X[colname]
         leaf_xranges, leaf_slopes, dx, dydx, pdpx, pdpy, ignored = \
             partial_dependence(X=X, y=y, colname=colname)
         #         print(f"Ignored for {colname} = {ignored}")
-        # record average abs of derivative divided by stddev to get z-score
-        stddevs[j] = np.std(np.abs(pdpy)) * (np.max(pdpx) - np.min(pdpx))
-        variations[j] = np.mean(np.abs(pdpy))
-        # stddevs[j] = np.std(np.abs(dydx))
-        # variations[j] = np.mean(np.abs(dydx))
+        # stddevs[j] = np.std(np.abs(pdpy))
+        print(colname, np.max(pdpx), np.min(pdpx))
+        variations[j] = np.mean(np.abs(pdpy)) * (np.max(pdpx) - np.min(pdpx))
         total_dy_mass += variations[j]
 
     normalized_variations = variations / total_dy_mass
 
     I = pd.DataFrame(data={'Feature': X.columns,
                            'Importance': normalized_variations,
-                           'Variation': variations,
-                           'sigma': stddevs})
+                           'AUC PDP':variations})
     I = I.set_index('Feature')
     I = I.sort_values('Importance', ascending=False)
 
     return I
-
-
 
 
 def plot_partials(X,y,eqn,yrange=(.5,1.5)):
@@ -106,12 +100,12 @@ def plot_partials(X,y,eqn,yrange=(.5,1.5)):
                      horizontalalignment='center')
 
 
-p=3
+p=5
 df, coeff, eqn = synthetic_poly_data(1000,p)
-print(df.head(3))
+print(df.describe())
 #
 X = df.drop('y', axis=1)
-X['noise'] = np.random.random_sample(size=len(X))
+#X['noise'] = np.random.random_sample(size=len(X))
 y = df['y']
 
 I = impact_importances(X, y)
@@ -129,70 +123,3 @@ plt.tight_layout()
 plt.savefig("/Users/parrt/Desktop/marginal.pdf", bbox_inches=0)
 plt.show()
 
-# JUNK
-
-'''
-def plot_marginal_dy(df):
-    fig, axes = plt.subplots(p+1,1,figsize=(7,7))
-
-
-    plt.suptitle('$'+eqn+'$', y=1.0)
-    y = df['y']
-    axes[0].scatter(range(len(df)), y, s=3)
-    axes[0].set_xlabel("$i$")
-    axes[0].set_ylabel("$y$")
-    dy = np.diff(y)
-    dy = np.concatenate([np.array([0]), dy])  # add back the 0 we lost
-    axes[0].plot(range(len(df)), dy, lw=.5, c='orange')
-    axes[0].text(len(df), 0, f"$\\frac{{\\partial y}}{{i}}$")
-    my = np.mean(np.abs(dy))
-    axes[0].set_title(f"mean $|\\frac{{\\partial y}}{{i}}|$ = {my:.2f}, $\\overline{{|y|}}$ = {np.mean(np.abs(y)):.2f}, $\\overline{{y-min(y)}}$ = {np.mean(y - min(y)):.1f}")
-
-    variations = np.zeros(shape=(p,))
-    total_mean_not_xj = 0.0
-    total_dy_mass = 0.0
-    for j in range(p):
-        # axes[j+1].set_title(f"$\\overline{{\\partial y/x_{j+1}}}$")
-        varname = f'x{j+1}'
-        axes[j+1].scatter(df[varname], y, s=3) # Plot marginal y
-        axes[j+1].set_xlabel(f'$x_{j+1}$')
-        df_by_xj = df.groupby(by=varname).mean().reset_index()
-        uniq_x = df_by_xj[varname]
-        avg_y = df_by_xj['y']
-        axes[j+1].plot(uniq_x, avg_y, lw=.5) # Plot avg y vs uniq x
-        df_sorted_by_xj = df_by_xj.sort_values(by=varname)
-
-        # Plot dy/dx_j
-        xj = df_sorted_by_xj[varname]
-        dy = np.diff(df_sorted_by_xj['y'])
-
-        segments = []
-        for i,delta in enumerate(dy):# / np.std(y)):
-            one_line = [(xj[i],0), (xj[i+1],delta)]
-            segments.append( one_line )
-        lines = LineCollection(segments, linewidths=.5, color='orange')
-        axes[j+1].add_collection(lines)
-        #axes[j+1].set_ylim(np.min(dy),np.max(y))
-        # axes[j+1].plot(df_sorted_by_xj[varname][:-1], dy, lw=.5, c='orange')
-        axes[j+1].text(max(xj), 0, f"$\\frac{{\\partial y}}{{x_{j+1}}}$")
-
-        # Draw partial derivative of partial dependence
-        leaf_xranges, leaf_slopes, dx, dydx, pdpx, pdpy, ignored = \
-            partial_dependence(X=df.drop('y', axis=1), y=y, colname=varname)
-        axes[j+1].plot(pdpx, pdpy,lw=.5, c='k')  # Plot PD_j
-        axes[j+1].text(max(pdpx), pdpy[-1], f"$PD_{j+1}$")
-        x_mass = np.mean(np.abs(pdpy))
-        dy_mass = np.mean(np.abs(dydx))# * (np.max(xj) - np.min(xj))
-        variations[j] = dy_mass
-        total_dy_mass += dy_mass
-        mean_not_xj = np.mean(np.abs(dy))# * (np.max(xj) - np.min(xj))
-        total_mean_not_xj += mean_not_xj
-
-        not_xj = set(range(1,p+1)) - {j+1}
-        not_xj = ','.join([f"x_{j}" for j in not_xj])
-        axes[j+1].set_title(f"mean $|\\frac{{\\partial y}}{{{not_xj}}}|$ = {mean_not_xj:.2f}, pd mass {x_mass:.2f}, partial mass {dy_mass:.3f}")
-        axes[j+1].set_ylabel("$y$")
-
-    print(f"Total means not x_j {total_mean_not_xj:.2f}, total dy mass {total_dy_mass:.2f}")
-    print("Variations", variations/total_dy_mass)
-'''
