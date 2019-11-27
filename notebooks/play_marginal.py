00bc51bc394f11f6054fa27aa7f7a9820f20833b
parrt <parrt@cs.usfca.edu>
2019-11-27 13:23:44 -0800

    added bootstrap

--- notebooks/play_marginal.py
+++ notebooks/play_marginal.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.utils import resample
 import matplotlib
-# matplotlib.use('TkAgg') # separate window
+import time
 
-# from stratx import featimp
 from stratx.partdep import *
 from rfpimp import plot_importances
 import rfpimp
@@ -26,52 +26,86 @@ palette = [
     "#b15928"
 ]
 
-def synthetic_poly_data(n, p):
+def synthetic_poly_data(n, p, noise=0):
     df = pd.DataFrame()
     # Add independent x variables in [0.0, 1.0)
     coeff = np.random.random_sample(size=p)*1 # get p random coefficients
     coeff = np.array([1,1,1,1,1,1,1,1,1])
     # coeff = np.array([5,10])
     for j in range(p):
-        df[f'x{j+1}'] = np.round(np.random.random_sample(size=n)*1,1) # shift x_i to right 2
+        df[f'x{j+1}'] = np.round(np.random.random_sample(size=n)*1,1)
+    if noise>0:
+        for j in range(noise):
+            df[f'noise{p+j+1}'] = np.round(np.random.random_sample(size=n)*1,1)
+
     #df['x3'] = df['x1']*np.round(np.random.random_sample(size=n),1) # copy x1 + noise
     # multiply coefficients x each column (var) and sum along columns
     yintercept = 10
     df['y'] = np.sum( [coeff[i]*df[f'x{i+1}'] for i in range(p)], axis=0 ) + yintercept
+    if noise>0:
+        df['y'] += np.sum( [df[f'noise{p + j + 1}'] for j in range(noise)] )
     terms = [f"{coeff[i]:.1f}x_{i+1}" for i in range(p)] + [f"{yintercept:.0f}"]
     eqn = "y = " + '+'.join(terms)
+    if noise>0:
+        eqn += ' + '
+        eqn += '+'.join([f'noise{p+j+1}' for j in range(noise)])
     return df, coeff, eqn
 
 
 def impact_importances(X: pd.DataFrame,
                        y: pd.Series,
-                       k: int=1) -> pd.DataFrame:
+                       n_samples=None,  # use all by default
+                       n_trials:int=3) -> pd.DataFrame:
     if not isinstance(X, pd.DataFrame):
         raise ValueError("Can only operate on dataframes at the moment")
 
-    p = X.shape[1]
-    variations = np.zeros(shape=(p,))
-    stddevs = np.zeros(shape=(p,))
-    total_dy_mass = 0.0
-    for j, colname in enumerate(X.columns):
-        # Ask stratx package for the partial dependence of y with respect to X[colname]
-        leaf_xranges, leaf_slopes, dx, dydx, pdpx, pdpy, ignored = \
-            partial_dependence(X=X, y=y, colname=colname)
-        #         print(f"Ignored for {colname} = {ignored}")
-        # stddevs[j] = np.std(np.abs(pdpy))
-        print(colname, np.max(pdpx), np.min(pdpx))
-        variations[j] = np.mean(np.abs(pdpy)) * (np.max(pdpx) - np.min(pdpx))
-        total_dy_mass += variations[j]
+    n,p = X.shape
+    imps = np.zeros(shape=(p, n_trials)) # track p var importances for ntrials; cols are trials
+    for i in range(n_trials):
+        bootstrap_sample_idxs = resample(range(n), n_samples=n_samples, replace=True)
+        X_, y_ = X.iloc[bootstrap_sample_idxs], y.iloc[bootstrap_sample_idxs]
+        imps[:,i] = impact_importances_(X_, y_)
 
-    normalized_variations = variations / total_dy_mass
+    avg_imps = np.mean(imps, axis=1)
+    stddev_imps = np.std(imps, axis=1)
 
     I = pd.DataFrame(data={'Feature': X.columns,
-                           'Importance': normalized_variations,
-                           'AUC PDP':variations})
+                           'Importance': avg_imps,
+                           "2Sigma":2*stddev_imps})
     I = I.set_index('Feature')
     I = I.sort_values('Importance', ascending=False)
 
     return I
+
+
+def impact_importances_(X: pd.DataFrame, y: pd.Series) -> np.ndarray:
+    if not isinstance(X, pd.DataFrame):
+        raise ValueError("Can only operate on dataframes at the moment")
+
+    p = X.shape[1]
+    auc_pdp = np.zeros(shape=(p,)) # area under PDP curve
+    total_pdpy_mass = 0.0
+    for j, colname in enumerate(X.columns):
+        # Ask stratx package for the partial dependence of y with respect to X[colname]
+        start = time.time()
+        leaf_xranges, leaf_slopes, dx, dydx, pdpx, pdpy, ignored = \
+            partial_dependence(X=X, y=y, colname=colname)
+        #         print(f"Ignored for {colname} = {ignored}")
+        stop = time.time()
+        print(f"PD time {(stop-start)*1000:.0f}ms")
+        auc_pdp[j] = np.mean(np.abs(pdpy)) * (np.max(pdpx) - np.min(pdpx))
+        total_pdpy_mass += auc_pdp[j]
+
+    normalized_auc_pdp = auc_pdp / total_pdpy_mass
+
+    return normalized_auc_pdp
+    # I = pd.DataFrame(data={'Feature': X.columns,
+    #                        'Importance': normalized_auc_pdp,
+    #                        'AUC PDP':auc_pdp})
+    # I = I.set_index('Feature')
+    # I = I.sort_values('Importance', ascending=False)
+    #
+    # return I
 
 
 def plot_partials(X,y,eqn,yrange=(.5,1.5)):
@@ -100,26 +134,22 @@ def plot_partials(X,y,eqn,yrange=(.5,1.5)):
                      horizontalalignment='center')
 
 
-p=5
-df, coeff, eqn = synthetic_poly_data(1000,p)
-print(df.describe())
-#
+p=3
+df, coeff, eqn = synthetic_poly_data(1000,p,noise=3)
+print(eqn)
 X = df.drop('y', axis=1)
 #X['noise'] = np.random.random_sample(size=len(X))
 y = df['y']
 
-I = impact_importances(X, y)
+I = impact_importances(X, y, n_samples=500, n_trials=5)
 print(I)
 plot_importances(I, imp_range=(0,1.0))
 #                  #color='#FEE192', #'#5D9CD2', #'#A99EFF',
 #                  # bgcolor='#F1F8FE'
 #                  )
 
-plot_partials(X, y, eqn, yrange=None)
-
-#plot_marginal_dy(df)
-#
-plt.tight_layout()
-plt.savefig("/Users/parrt/Desktop/marginal.pdf", bbox_inches=0)
+#plot_partials(X, y, eqn, yrange=None)
+# plt.tight_layout()
+# plt.savefig("/Users/parrt/Desktop/marginal.pdf", bbox_inches=0)
 plt.show()
 
