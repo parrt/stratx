1d59fbfda630261308e49819f615aeef15c6b87d
parrt <parrt@cs.usfca.edu>
2020-01-13 11:15:57 -0800

    add new catstrat plot

--- articles/imp/genfigs/show_catpd_variance.py
+++ articles/imp/genfigs/show_catpd_variance.py
@@ -6,40 +6,28 @@ use_oob=False
 metric = mean_absolute_error
 n = 25000
 
+def toy_weather_data_():
+    def temp(x): return np.sin((x+365/2)*(2*np.pi)/365)
+    def noise(state): return np.random.normal(-5, 5, sum(df['state'] == state))
+
+    df = pd.DataFrame()
+    df['dayofyear'] = range(1,365+1)
+    df['state'] = np.random.choice(['CA','CO','AZ','WA'], len(df))
+    df['temperature'] = temp(df['dayofyear'])
+    df.loc[df['state']=='CA','temperature'] = 70 + df.loc[df['state']=='CA','temperature'] * noise('CA')
+    df.loc[df['state']=='CO','temperature'] = 40 + df.loc[df['state']=='CO','temperature'] * noise('CO')
+    df.loc[df['state']=='AZ','temperature'] = 90 + df.loc[df['state']=='AZ','temperature'] * noise('AZ')
+    df.loc[df['state']=='WA','temperature'] = 60 + df.loc[df['state']=='WA','temperature'] * noise('WA')
+    return df
+
+
+
 X, y = load_bulldozer()
 
 # Most recent timeseries data is more relevant so get big recent chunk
 # then we can sample from that to get n
 X = X.iloc[-50_000:]
 y = y.iloc[-50_000:]
-
-n_trials=5
-colname = "YearMade"
-colname = 'ModelID'
-colname = "ProductSize"
-targetname="SalePrice"
-min_samples_leaf=5
-min_slopes_per_x=5
-all_avg_per_cat = []
-style='level'
-show_xticks=False
-show_x_counts=True
-barchart_size = 0.20
-barchar_alpha = 0.9
-label_fontsize = 10
-ticklabel_fontsize = 10
-pdp_marker_size=3
-pdp_marker_alpha = .6
-fontname = 'Arial'
-yrange = None
-show_impact = True
-impact_color = '#D73028'
-figsize=(5,3)
-show_xlabel = True
-show_ylabel = True
-title=None
-title_fontsize=12
-
 
 idxs = resample(range(50_000), n_samples=n, replace=False)
 X, y = X.iloc[idxs], y.iloc[idxs]  # get sample from last part of time range
@@ -50,184 +38,9 @@ X, y = X.iloc[idxs], y.iloc[idxs]  # get sample from last part of time range
 # n = len(X)
 
 print(f"n={n}")
-# catcodes, _, catcode2name = getcats(X, colname, None)
 
-uniq_catcodes = np.unique(X[colname])
-max_catcode = max(uniq_catcodes)
+#title = f"n={n}\nstd(mean(abs(y)))={std_imp:.3f}\nmin_samples_leaf={min_samples_leaf}\nmin_slopes_per_x={min_slopes_per_x}", fontsize=9
+plot_catstratpd(X, y, colname='ProductSize', targetname="SalePrice")
 
-X_col = X[colname]
-
-def avg_pd_catvalues(all_avg_per_cat):
-    m = np.zeros(shape=(max_catcode+1,))
-    c = np.zeros(shape=(max_catcode+1,), dtype=int)
-
-    # For each unique catcode, sum and count avg_per_cat values found among trials
-    for i in range(n_trials):
-        avg_per_cat = all_avg_per_cat[i]
-        catcodes = np.where(~np.isnan(avg_per_cat))[0]
-        for code in catcodes:
-            m[code] += avg_per_cat[code]
-            c[code] += 1
-    # Convert to average value per cat
-    for code in np.where(m!=0)[0]:
-        m[code] /= c[code]
-    m = np.where(c==0, np.nan, m) # cats w/o values should be nan, not 0
-    return m
-
-for i in range(n_trials):
-    print(i)
-    # idxs = resample(range(n), n_samples=n, replace=True) # bootstrap
-    idxs = resample(range(n), n_samples=int(n*2/3), replace=False) # subset
-    X_, y_ = X.iloc[idxs], y.iloc[idxs]
-
-    leaf_histos, avg_per_cat, ignored = \
-        cat_partial_dependence(X_, y_,
-                               max_catcode=np.max(X_col),
-                               colname=colname,
-                               n_trees=1,
-                               min_samples_leaf=min_samples_leaf,
-                               max_features=1.0,
-                               bootstrap=False)
-
-    all_avg_per_cat.append( avg_per_cat )
-
-
-combined_avg_per_cat = avg_pd_catvalues(all_avg_per_cat)
-
-impacts = []
-for i in range(n_trials):
-    avg_per_cat = all_avg_per_cat[i]
-    abs_avg_per_cat = np.abs(avg_per_cat[~np.isnan(avg_per_cat)])
-    trial_uniq_cats = np.where(~np.isnan(avg_per_cat))[0]
-    cat_counts = [len(np.where(X_col == cat)[0]) for cat in trial_uniq_cats]
-    impact = np.sum(np.abs(abs_avg_per_cat * cat_counts)) / np.sum(cat_counts)
-    impacts.append(impact)
-impact_order = np.argsort(impacts)
-print(impacts)
-print(impact_order)
-avg_impact = np.mean(impacts)
-print(avg_impact)
-
-fig, ax = plt.subplots(1, 1, figsize=figsize)
-
-cmap = plt.get_cmap('coolwarm')
-colors=cmap(np.linspace(0, 1, num=n_trials))
-min_y = 9999999999999
-max_y = -min_y
-for i in range(n_trials):
-    avg_per_cat = all_avg_per_cat[i]
-    if np.nanmin(avg_per_cat) < min_y:
-        min_y = np.nanmin(avg_per_cat)
-    if np.nanmax(avg_per_cat) > max_y:
-        max_y = np.nanmax(avg_per_cat)
-    trial_catcodes = np.where(~np.isnan(avg_per_cat))[0]
-    print("catcodes", trial_catcodes, "range", min(trial_catcodes), max(trial_catcodes))
-    # walk each potential catcode but plot with x in 0..maxcode+1; ignore nan avg_per_cat values
-    xloc = -1 # go from 0 but must count nan entries
-    collect_cats = []
-    collect_deltas = []
-    for cat in uniq_catcodes:
-        cat_delta = avg_per_cat[cat]
-        xloc += 1
-        if np.isnan(cat_delta): continue
-        # ax.plot([xloc - .15, xloc + .15], [cat_delta] * 2, c=colors[impact_order[i]], linewidth=1)
-        collect_cats.append(xloc)
-        collect_deltas.append(cat_delta)
-    print("Got to xloc", xloc, "len(trial_catcodes)", len(trial_catcodes), "len(catcodes)", len(uniq_catcodes))
-    # ax.scatter(collect_cats, collect_deltas, c=mpl.colors.rgb2hex(colors[impact_order[i]]),
-    #            s=pdp_marker_size, alpha=pdp_marker_alpha)
-    ax.plot(collect_cats, collect_deltas, '.', c=mpl.colors.rgb2hex(colors[impact_order[i]]),
-            markersize=pdp_marker_size, alpha=pdp_marker_alpha)
-
-# show 0 line
-ax.plot([0,len(uniq_catcodes)], [0,0], '--', c='grey', lw=.5)
-
-# Show avg line
-xloc = 0
-avg_delta = []
-for cat in uniq_catcodes:
-    cat_delta = combined_avg_per_cat[cat]
-    avg_delta.append(cat_delta)
-    xloc += 1
-
-ax.plot(range(len(uniq_catcodes)), avg_delta, '.', c='k', markersize=pdp_marker_size + 1)
-
-if show_impact:
-    ax.text(0.5, .94, f"Impact {avg_impact:.2f}",
-            horizontalalignment='center',
-            fontsize=label_fontsize, fontname=fontname,
-            transform=ax.transAxes,
-            color=impact_color)
-
-if show_x_counts:
-    combined_uniq_cats = np.where(~np.isnan(combined_avg_per_cat))[0]
-    _, cat_counts = np.unique(X_col[np.isin(X_col, combined_uniq_cats)], return_counts=True)
-    # x_width = len(uniq_catcodes)
-    # count_bar_width = x_width / len(pdpx)
-    # if count_bar_width/x_width < 0.002:
-    #     count_bar_width = x_width * 0.002 # don't make them so skinny they're invisible
-    count_bar_width=1
-    ax2 = ax.twinx()
-    # scale y axis so the max count height is 10% of overall chart
-    ax2.set_ylim(0, max(cat_counts) * 1/barchart_size)
-    # draw just 0 and max count
-    ax2.yaxis.set_major_locator(plt.FixedLocator([0, max(cat_counts)]))
-    ax2.bar(x=range(len(combined_uniq_cats)), height=cat_counts, width=count_bar_width,
-            facecolor='#BABABA', align='edge', alpha=barchar_alpha)
-    ax2.set_ylabel(f"$x$ point count", labelpad=-12, fontsize=label_fontsize,
-                   fontstretch='extra-condensed',
-                   fontname=fontname)
-    # shift other y axis down barchart_size to make room
-    if yrange is not None:
-        ax.set_ylim(yrange[0]-(yrange[1]-yrange[0])*barchart_size, yrange[1])
-    else:
-        ax.set_ylim(min_y-(max_y-min_y)*barchart_size, max_y)
-    # ax2.set_xticks(range(len(uniq_catcodes)))
-    # ax2.set_xticklabels([])
-    plt.setp(ax2.get_xticklabels(), visible=False)
-    # ax2.tick_params(axis='both', which='major', labelsize=ticklabel_fontsize)
-    # for tick in ax2.get_xticklabels():
-    #     tick.set_visible(False)
-    for tick in ax2.get_yticklabels():
-        tick.set_fontname(fontname)
-    ax2.spines['top'].set_linewidth(.5)
-    ax2.spines['right'].set_linewidth(.5)
-    ax2.spines['left'].set_linewidth(.5)
-    ax2.spines['bottom'].set_linewidth(.5)
-
-# np.where(combined_avg_per_cat!=0)[0]
-if show_xticks:
-    ax.set_xticks(range(len(uniq_catcodes)))
-    ax.set_xticklabels(uniq_catcodes)
-else:
-    ax.set_xticks([])
-    ax.set_xticklabels([])
-
-if show_xlabel:
-    ax.set_xlabel(colname, fontsize=label_fontsize, fontname=fontname)
-if show_ylabel:
-    ax.set_ylabel(targetname, fontsize=label_fontsize, fontname=fontname)
-if title is not None:
-    ax.set_title(title, fontsize=title_fontsize, fontname=fontname)
-
-for tick in ax.get_xticklabels():
-    tick.set_fontname(fontname)
-for tick in ax.get_yticklabels():
-    tick.set_fontname(fontname)
-
-# if show_xticks: # sometimes too many
-#     ax.set_xticklabels(catcode2name[sorted_catcodes])
-#     ax.tick_params(axis='x', which='major', labelsize=ticklabel_fontsize)
-# else:
-#     ax.set_xticklabels([])
-#     ax.tick_params(axis='x', which='major', labelsize=ticklabel_fontsize, bottom=False)
-# ax.tick_params(axis='y', which='major', labelsize=ticklabel_fontsize)
-
-ax.spines['top'].set_linewidth(.5)
-ax.spines['right'].set_linewidth(.5)
-ax.spines['left'].set_linewidth(.5)
-ax.spines['bottom'].set_linewidth(.5)
-
-# ax.set_title(f"n={n}\nstd(mean(abs(y)))={std_imp:.3f}\nmin_samples_leaf={min_samples_leaf}\nmin_slopes_per_x={min_slopes_per_x}", fontsize=9)
 plt.savefig(f"/Users/parrt/Desktop/catstrat-mu.pdf", pad_inches=0)
 plt.show()
--- stratx/partdep.py
+++ stratx/partdep.py
@@ -1173,8 +1173,234 @@ def cat_partial_dependence(X, y,
     return leaf_histos, avg_per_cat, ignored
 
 
-# only works for ints, not floats
 def plot_catstratpd(X, y,
+                    colname,  # X[colname] expected to be numeric codes
+                    targetname,
+                    catnames=None,  # map of catcodes to catnames; converted to map if sequence passed
+                    # must pass dict or series if catcodes are not 1..n contiguous
+                    # None implies use np.unique(X[colname]) values
+                    # Must be 0-indexed list of names if list
+                    n_trials=10,
+                    ax=None,
+                    sort='ascending',
+                    n_trees=1,
+                    min_samples_leaf=10,
+                    max_features=1.0,
+                    bootstrap=False,
+                    yrange=None,
+                    title=None,
+                    supervised=True,
+                    use_weighted_avg=False,
+                    show_impact=False,
+                    show_all_deltas=True,
+                    show_x_counts=True,
+                    impact_color='#D73028',
+                    alpha=.15,
+                    color='#2c7fb8',
+                    pdp_marker_size=3,
+                    pdp_marker_alpha=.6,
+                    marker_size=5,
+                    pdp_color='black',
+                    fontname='Arial',
+                    title_fontsize=11,
+                    label_fontsize=10,
+                    barchart_size=0.20,
+                    barchar_alpha=0.9,
+                    ticklabel_fontsize=10,
+                    style: ('strip', 'scatter') = 'strip',
+                    min_y_shifted_to_zero=True,
+                    # easier to read if values are relative to 0 (usually); do this for high cardinality cat vars
+                    show_xlabel=True,
+                    show_ylabel=True,
+                    verbose=False,
+                    figsize=(5,3)):
+    """
+    Warning: cat columns are assumed to be label encoded as unique integers. This
+    function uses the cat code as a raw index internally. So if you have two cat
+    codes 1 and 1000, this function allocates internal arrays of size 1000+1.
+
+    only works for ints, not floats
+    """
+
+    all_avg_per_cat = []
+
+    uniq_catcodes = np.unique(X[colname])
+    max_catcode = max(uniq_catcodes)
+
+    X_col = X[colname]
+    n = len(X_col)
+
+    def avg_pd_catvalues(all_avg_per_cat):
+        m = np.zeros(shape=(max_catcode+1,))
+        c = np.zeros(shape=(max_catcode+1,), dtype=int)
+
+        # For each unique catcode, sum and count avg_per_cat values found among trials
+        for i in range(n_trials):
+            avg_per_cat = all_avg_per_cat[i]
+            catcodes = np.where(~np.isnan(avg_per_cat))[0]
+            for code in catcodes:
+                m[code] += avg_per_cat[code]
+                c[code] += 1
+        # Convert to average value per cat
+        for code in np.where(m!=0)[0]:
+            m[code] /= c[code]
+        m = np.where(c==0, np.nan, m) # cats w/o values should be nan, not 0
+        return m
+
+    ignored = 0
+    for i in range(n_trials):
+        print(i)
+        # idxs = resample(range(n), n_samples=n, replace=True) # bootstrap
+        idxs = resample(range(n), n_samples=int(n*2/3), replace=False) # subset
+        X_, y_ = X.iloc[idxs], y.iloc[idxs]
+
+        leaf_histos, avg_per_cat, ignored_ = \
+            cat_partial_dependence(X_, y_,
+                                   max_catcode=np.max(X_col),
+                                   colname=colname,
+                                   n_trees=1,
+                                   min_samples_leaf=min_samples_leaf,
+                                   max_features=1.0,
+                                   bootstrap=False)
+        ignored += ignored_
+        all_avg_per_cat.append( avg_per_cat )
+
+    ignored /= n_trials # average number of x values ignored across trials
+
+    combined_avg_per_cat = avg_pd_catvalues(all_avg_per_cat)
+
+    impacts = []
+    for i in range(n_trials):
+        avg_per_cat = all_avg_per_cat[i]
+        abs_avg_per_cat = np.abs(avg_per_cat[~np.isnan(avg_per_cat)])
+        trial_uniq_cats = np.where(~np.isnan(avg_per_cat))[0]
+        cat_counts = [len(np.where(X_col == cat)[0]) for cat in trial_uniq_cats]
+        impact = np.sum(np.abs(abs_avg_per_cat * cat_counts)) / np.sum(cat_counts)
+        impacts.append(impact)
+    impact_order = np.argsort(impacts)
+    print(impacts)
+    print(impact_order)
+    avg_impact = np.mean(impacts)
+    print(avg_impact)
+
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    cmap = plt.get_cmap('coolwarm')
+    colors=cmap(np.linspace(0, 1, num=n_trials))
+    min_y = 9999999999999
+    max_y = -min_y
+    for i in range(n_trials):
+        avg_per_cat = all_avg_per_cat[i]
+        if np.nanmin(avg_per_cat) < min_y:
+            min_y = np.nanmin(avg_per_cat)
+        if np.nanmax(avg_per_cat) > max_y:
+            max_y = np.nanmax(avg_per_cat)
+        trial_catcodes = np.where(~np.isnan(avg_per_cat))[0]
+        print("catcodes", trial_catcodes, "range", min(trial_catcodes), max(trial_catcodes))
+        # walk each potential catcode but plot with x in 0..maxcode+1; ignore nan avg_per_cat values
+        xloc = -1 # go from 0 but must count nan entries
+        collect_cats = []
+        collect_deltas = []
+        for cat in uniq_catcodes:
+            cat_delta = avg_per_cat[cat]
+            xloc += 1
+            if np.isnan(cat_delta): continue
+            # ax.plot([xloc - .15, xloc + .15], [cat_delta] * 2, c=colors[impact_order[i]], linewidth=1)
+            collect_cats.append(xloc)
+            collect_deltas.append(cat_delta)
+        print("Got to xloc", xloc, "len(trial_catcodes)", len(trial_catcodes), "len(catcodes)", len(uniq_catcodes))
+        # ax.scatter(collect_cats, collect_deltas, c=mpl.colors.rgb2hex(colors[impact_order[i]]),
+        #            s=pdp_marker_size, alpha=pdp_marker_alpha)
+        ax.plot(collect_cats, collect_deltas, '.', c=mpl.colors.rgb2hex(colors[impact_order[i]]),
+                markersize=pdp_marker_size, alpha=pdp_marker_alpha)
+
+    # show 0 line
+    ax.plot([0,len(uniq_catcodes)], [0,0], '--', c='grey', lw=.5)
+
+    # Show avg line
+    xloc = 0
+    avg_delta = []
+    for cat in uniq_catcodes:
+        cat_delta = combined_avg_per_cat[cat]
+        avg_delta.append(cat_delta)
+        xloc += 1
+
+    ax.plot(range(len(uniq_catcodes)), avg_delta, '.', c='k', markersize=pdp_marker_size + 1)
+
+    if show_impact:
+        ax.text(0.5, .94, f"Impact {avg_impact:.2f}",
+                horizontalalignment='center',
+                fontsize=label_fontsize, fontname=fontname,
+                transform=ax.transAxes,
+                color=impact_color)
+
+    if show_x_counts:
+        combined_uniq_cats = np.where(~np.isnan(combined_avg_per_cat))[0]
+        _, cat_counts = np.unique(X_col[np.isin(X_col, combined_uniq_cats)], return_counts=True)
+        # x_width = len(uniq_catcodes)
+        # count_bar_width = x_width / len(pdpx)
+        # if count_bar_width/x_width < 0.002:
+        #     count_bar_width = x_width * 0.002 # don't make them so skinny they're invisible
+        count_bar_width=1
+        ax2 = ax.twinx()
+        # scale y axis so the max count height is 10% of overall chart
+        ax2.set_ylim(0, max(cat_counts) * 1/barchart_size)
+        # draw just 0 and max count
+        ax2.yaxis.set_major_locator(plt.FixedLocator([0, max(cat_counts)]))
+        ax2.bar(x=range(len(combined_uniq_cats)), height=cat_counts, width=count_bar_width,
+                facecolor='#BABABA', align='edge', alpha=barchar_alpha)
+        ax2.set_ylabel(f"$x$ point count", labelpad=-12, fontsize=label_fontsize,
+                       fontstretch='extra-condensed',
+                       fontname=fontname)
+        # shift other y axis down barchart_size to make room
+        if yrange is not None:
+            ax.set_ylim(yrange[0]-(yrange[1]-yrange[0])*barchart_size, yrange[1])
+        else:
+            ax.set_ylim(min_y-(max_y-min_y)*barchart_size, max_y)
+        # ax2.set_xticks(range(len(uniq_catcodes)))
+        # ax2.set_xticklabels([])
+        plt.setp(ax2.get_xticklabels(), visible=False)
+        # ax2.tick_params(axis='both', which='major', labelsize=ticklabel_fontsize)
+        # for tick in ax2.get_xticklabels():
+        #     tick.set_visible(False)
+        for tick in ax2.get_yticklabels():
+            tick.set_fontname(fontname)
+        ax2.spines['top'].set_linewidth(.5)
+        ax2.spines['right'].set_linewidth(.5)
+        ax2.spines['left'].set_linewidth(.5)
+        ax2.spines['bottom'].set_linewidth(.5)
+
+    # np.where(combined_avg_per_cat!=0)[0]
+    if show_xlabel:
+        ax.set_xticks(range(len(uniq_catcodes)))
+        ax.set_xticklabels(uniq_catcodes)
+        ax.set_xlabel(colname, fontsize=label_fontsize, fontname=fontname)
+    else:
+        ax.set_xticks([])
+        ax.set_xticklabels([])
+
+    if show_ylabel:
+        ax.set_ylabel(targetname, fontsize=label_fontsize, fontname=fontname)
+    if title is not None:
+        ax.set_title(title, fontsize=title_fontsize, fontname=fontname)
+
+    for tick in ax.get_xticklabels():
+        tick.set_fontname(fontname)
+    for tick in ax.get_yticklabels():
+        tick.set_fontname(fontname)
+
+    if title is not None:
+        ax.set_title(title, fontsize=title_fontsize, fontname=fontname)
+
+    ax.spines['top'].set_linewidth(.5)
+    ax.spines['right'].set_linewidth(.5)
+    ax.spines['left'].set_linewidth(.5)
+    ax.spines['bottom'].set_linewidth(.5)
+
+    return uniq_catcodes, combined_avg_per_cat
+
+# only works for ints, not floats
+def plot_catstratpd_OLD(X, y,
                     colname,  # X[colname] expected to be numeric codes
                     targetname,
                     catnames=None,  # map of catcodes to catnames; converted to map if sequence passed
